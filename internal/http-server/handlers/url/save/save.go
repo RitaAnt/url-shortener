9963471ff75628e54443288a0dc29cba688fdf2f
ritamorgunova <kolotvinova03@bk.ru>
2025-04-23 19:28:33 +0300

    refactoring: safe validation

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -23,14 +23,12 @@ type Response struct {
 	Alias string `json:"alias,omitempty"`
 }
 
-const aliasLength = 6
-
 //go:generate mockery --name=URLSaver
 type URLSaver interface {
 	SaveURL(urlToSave string, alias string) error
 }
 
-func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+func New(log *slog.Logger, urlSaver URLSaver, aliasLength int) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		const fn = "handlers.url.save.New"
 
@@ -39,7 +37,6 @@ func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 			slog.String("request_id", middleware.GetReqID(r.Context())))
 
 		var req Request
-
 		err := render.DecodeJSON(r.Body, &req)
 		if err != nil {
 			log.Error("failed to decode request body", sl.Err(err))
@@ -47,14 +44,19 @@ func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 			return
 		}
 
-		log.Info("request body decoded", slog.Any("request", req))
-
+		log.Debug("request body decoded", slog.Any("request", req))
 		validate := validator.New()
 
 		if err := validate.Struct(req); err != nil {
-			validateErr := err.(validator.ValidationErrors)
-			log.Error("invalid request", sl.Err(err))
-			render.JSON(w, r, response.ValidationError(validateErr))
+			var validateErr validator.ValidationErrors
+			if errors.As(err, &validateErr) {
+				log.Error("invalid request", sl.Err(err))
+				render.JSON(w, r, response.ValidationError(validateErr))
+				return
+			}
+
+			log.Error("validation failed with unknown error", sl.Err(err))
+			render.JSON(w, r, response.Error("validation failed"))
 			return
 		}
 
@@ -64,21 +66,20 @@ func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 		}
 
 		err = urlSaver.SaveURL(req.Url, alias)
-		if errors.Is(err, storage.ErrURLExists) {
-			log.Info("url already exists", slog.String("url", req.Url))
-
-			render.JSON(w, r, response.Error("url already exists"))
-
-			return
-		}
 		if err != nil {
+			if errors.Is(err, storage.ErrURLExists) {
+				log.Info("url already exists", slog.String("url", req.Url))
+
+				render.JSON(w, r, response.Error("url already exists"))
+
+				return
+			}
 			log.Error("failed to save url", sl.Err(err))
 			render.JSON(w, r, response.Error("failed to save url"))
 			return
 		}
 
 		log.Info("url saved", slog.String("url", req.Url))
-
 		responseOk(w, r, alias)
 	}
 
