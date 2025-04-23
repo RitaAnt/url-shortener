3bc4682d5f63c9a6a94911f099e324320d91e092
ritamorgunova <kolotvinova03@bk.ru>
2025-04-23 19:31:15 +0300

    handler delete

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/http-server/handlers/delete"
 	"url-shortener/internal/http-server/handlers/redirect"
 	"url-shortener/internal/http-server/handlers/url/save"
 	mwLogger "url-shortener/internal/http-server/middleware/logger"
@@ -45,6 +46,7 @@ func main() {
 
 	router.Post("/url", save.New(log, storage, cfg.AliasLength))
 	router.Get("/{alias}", redirect.New(log, storage))
+	router.Delete("/{alias}", delete.New(log, storage))
 
 	log.Info("starting server", slog.String("address", cfg.Address))
 
